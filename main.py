11dd81f65622c609edbdb1a67d4fa9530c3a7871
Filip Michalski <filip.michalski92@gmail.com>
2019-08-21 23:58:21 +0200

    refactored small functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 from datetime import datetime
 
 
@@ -13,12 +14,10 @@ def get_computer_name():
 def get_current_time_in_hex():
     current_time = get_current_time()
     time_in_hex = []
-    i = 0
 
-    for letters in current_time:
+    for i in range(0, len(current_time)):
         x = (hex(ord(current_time[i])))[2:].upper()
         time_in_hex.append(x)
-        i += 1
 
     return time_in_hex
 
@@ -26,12 +25,10 @@ def get_current_time_in_hex():
 def get_computer_name_in_hex():
     computer_name = get_computer_name()
     computer_name_in_hex = []
-    i = 0
 
-    for letters in computer_name:
+    for i in range(0, len(computer_name)):
         x = (hex(ord(computer_name[i])))[2:].upper()
         computer_name_in_hex.append(x)
-        i += 1
 
     return computer_name_in_hex
 
@@ -39,33 +36,54 @@ def get_computer_name_in_hex():
 def create_production_hex(file_name):
     f = open(file_name, "w+")
 
-    f.write(":204000000000000000000000000000000000000000000000000000000000000000000000A0\n")
-    f.write(":20402000000000000000000000000000000000000000000000000000000000000000000080\n")
-    f.write(":20404000000000000000000000000000000000000000000000000000000000000000000060\n")
-    f.write(":20406000000000000000000000000000000000000000000000000000000000000000000040\n")
-    f.write(":20408000000000000000000000000000000000000000000000000000000000000000000020\n")
-    f.write(":2040A000000000000000000000000000000000000000000000000000000000000000000000\n")
-    f.write(":2040C0000000000000000000000000000000000000000000000000000000000000000000E0\n")
-    f.write(":2040E0000000000000000000000000000000000000000000000000000000000000000000C0\n")
-    f.write(":2041000000000000000000000000000000000000000000000000000000000000000000009F\n")
-    f.write(":2041200000000000000000000000000000000000000000000000000000000000000000007F\n")
-    f.write(":2041400000000000000000000000000000000000000000000000000000000000000000005F\n")
-    f.write(":2041600000000000000000000000000000000000000000000000000000000000000000003F\n")
-    f.write(":2041800000000000000000000000000000000000000000000000000000000000000000001F\n")
-    f.write(":2041A0000000000000000000000000000000000000000000000000000000000000000000FF\n")
-    f.write(":2041C0000000000000000000000000000000000000000000000000000000000000000000DF\n")
-    f.write(":2041E0000000000000000000000000000000000000000000000000000000000000000000BF\n")
-    f.write(":2042000000000000000000000000000000000000000000000000000000000000000000009E\n")
-    f.write(":2042200000000000000000000000000000000000000000000000000000000000000000007E\n")
-    f.write(":2042400000000000000000000000000000000000000000000000000000000000000000005E\n")
-    f.write(":2042600000000000000000000000000000000000000000000000000000000000000000003E\n")
+    f.write(
+        ":204000000000000000000000000000000000000000000000000000000000000000000000A0\n")
+    f.write(
+        ":20402000000000000000000000000000000000000000000000000000000000000000000080\n")
+    f.write(
+        ":20404000000000000000000000000000000000000000000000000000000000000000000060\n")
+    f.write(
+        ":20406000000000000000000000000000000000000000000000000000000000000000000040\n")
+    f.write(
+        ":20408000000000000000000000000000000000000000000000000000000000000000000020\n")
+    f.write(
+        ":2040A000000000000000000000000000000000000000000000000000000000000000000000\n")
+    f.write(
+        ":2040C0000000000000000000000000000000000000000000000000000000000000000000E0\n")
+    f.write(
+        ":2040E0000000000000000000000000000000000000000000000000000000000000000000C0\n")
+    f.write(
+        ":2041000000000000000000000000000000000000000000000000000000000000000000009F\n")
+    f.write(
+        ":2041200000000000000000000000000000000000000000000000000000000000000000007F\n")
+    f.write(
+        ":2041400000000000000000000000000000000000000000000000000000000000000000005F\n")
+    f.write(
+        ":2041600000000000000000000000000000000000000000000000000000000000000000003F\n")
+    f.write(
+        ":2041800000000000000000000000000000000000000000000000000000000000000000001F\n")
+    f.write(
+        ":2041A0000000000000000000000000000000000000000000000000000000000000000000FF\n")
+    f.write(
+        ":2041C0000000000000000000000000000000000000000000000000000000000000000000DF\n")
+    f.write(
+        ":2041E0000000000000000000000000000000000000000000000000000000000000000000BF\n")
+    f.write(
+        ":2042000000000000000000000000000000000000000000000000000000000000000000009E\n")
+    f.write(
+        ":2042200000000000000000000000000000000000000000000000000000000000000000007E\n")
+    f.write(
+        ":2042400000000000000000000000000000000000000000000000000000000000000000005E\n")
+    f.write(
+        ":2042600000000000000000000000000000000000000000000000000000000000000000003E\n")
     f.write(":00000001FF\n")
 
     f.close()
 
 
+# adds missing zero's to the given line to satisfy the hex file
 def add_missing_zeros(line):
-    required_len = 73
+    required_len = 73  # number of characters in a line in a hex file
 
     while len(line) < required_len:
         line += '0'
@@ -74,8 +92,9 @@ def add_missing_zeros(line):
 
 
 def calculate_hex_checksum(line):
-    line_to_calculate = line[1:]
-    temp2 = [line_to_calculate[i:i + 2] for i in range(0, len(line_to_calculate), 2)]
+    line_to_calculate = line[1:]  # remove ':' from the line
+    temp2 = [line_to_calculate[i:i + 2]
+             for i in range(0, len(line_to_calculate), 2)]
     sum = 0
     i = 0
     for n in temp2:
@@ -89,24 +108,28 @@ def calculate_hex_checksum(line):
     return sum
 
 
-file = str(sys.argv[1])
+def main():
+    file = str(sys.argv[1])
 
-if os.path.isfile(file) == 0:
-    create_production_hex(file)
+    if os.path.isfile(file) == 0:
+        create_production_hex(file)
 
-with open(file) as f:
-    file_content = f.readlines()
+    with open(file) as f:
+        file_content = f.readlines()
 
-file_content[1] = ':20402000' + ''.join(map(str, get_computer_name_in_hex()))
-file_content[2] = ':20404000' + ''.join(map(str, get_current_time_in_hex()))
+    file_content[1] = ':20402000' + \
+        ''.join(map(str, get_computer_name_in_hex()))
+    file_content[2] = ':20404000' + \
+        ''.join(map(str, get_current_time_in_hex()))
 
-file_content[1] = add_missing_zeros(file_content[1])
-file_content[2] = add_missing_zeros(file_content[2])
-file_content[1] += calculate_hex_checksum(file_content[1])
-file_content[2] += calculate_hex_checksum(file_content[2])
+    for i in range(1, 3):
+        file_content[i] = add_missing_zeros(file_content[i])
+        file_content[i] += calculate_hex_checksum(file_content[i])
+        file_content[i] += '\n'
 
-file_content[1] += '\n'
-file_content[2] += '\n'
+    with open(file, 'w') as f:
+        f.writelines(file_content)
 
-with open(file, 'w') as f:
-    f.writelines(file_content)
+
+if __name__ == "__main__":
+    main()
