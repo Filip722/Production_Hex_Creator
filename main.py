5e02701146269652d71d8a62e1f0678bfaa9fe09
Filip Michalski <filip.michalski92@gmail.com>
2019-08-22 22:45:35 +0200

    changed for loops from range to enumerate + file variable name change

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def get_current_time_in_hex():
     current_time = get_current_time()
     time_in_hex = []
 
-    for i in range(0, len(current_time)):
-        x = (hex(ord(current_time[i])))[2:].upper()
-        time_in_hex.append(x)
+    for i, _ in enumerate(current_time):
+        hex = (hex(ord(current_time[i])))[2:].upper()
+        time_in_hex.append(hex)
 
     return time_in_hex
 
@@ -26,62 +26,61 @@ def get_computer_name_in_hex():
     computer_name = get_computer_name()
     computer_name_in_hex = []
 
-    for i in range(0, len(computer_name)):
-        x = (hex(ord(computer_name[i])))[2:].upper()
-        computer_name_in_hex.append(x)
+    for i, _ in enumerate(computer_name):
+        hex = (hex(ord(computer_name[i])))[2:].upper()
+        computer_name_in_hex.append(hex)
 
     return computer_name_in_hex
 
 
 def create_production_hex(file_name):
-    f = open(file_name, "w+")
+    file = open(file_name, "w+")
 
-    f.write(
+    file.write(
         ":204000000000000000000000000000000000000000000000000000000000000000000000A0\n")
-    f.write(
+    file.write(
         ":20402000000000000000000000000000000000000000000000000000000000000000000080\n")
-    f.write(
+    file.write(
         ":20404000000000000000000000000000000000000000000000000000000000000000000060\n")
-    f.write(
+    file.write(
         ":20406000000000000000000000000000000000000000000000000000000000000000000040\n")
-    f.write(
+    file.write(
         ":20408000000000000000000000000000000000000000000000000000000000000000000020\n")
-    f.write(
+    file.write(
         ":2040A000000000000000000000000000000000000000000000000000000000000000000000\n")
-    f.write(
+    file.write(
         ":2040C0000000000000000000000000000000000000000000000000000000000000000000E0\n")
-    f.write(
+    file.write(
         ":2040E0000000000000000000000000000000000000000000000000000000000000000000C0\n")
-    f.write(
+    file.write(
         ":2041000000000000000000000000000000000000000000000000000000000000000000009F\n")
-    f.write(
+    file.write(
         ":2041200000000000000000000000000000000000000000000000000000000000000000007F\n")
-    f.write(
+    file.write(
         ":2041400000000000000000000000000000000000000000000000000000000000000000005F\n")
-    f.write(
+    file.write(
         ":2041600000000000000000000000000000000000000000000000000000000000000000003F\n")
-    f.write(
+    file.write(
         ":2041800000000000000000000000000000000000000000000000000000000000000000001F\n")
-    f.write(
+    file.write(
         ":2041A0000000000000000000000000000000000000000000000000000000000000000000FF\n")
-    f.write(
+    file.write(
         ":2041C0000000000000000000000000000000000000000000000000000000000000000000DF\n")
-    f.write(
+    file.write(
         ":2041E0000000000000000000000000000000000000000000000000000000000000000000BF\n")
-    f.write(
+    file.write(
         ":2042000000000000000000000000000000000000000000000000000000000000000000009E\n")
-    f.write(
+    file.write(
         ":2042200000000000000000000000000000000000000000000000000000000000000000007E\n")
-    f.write(
+    file.write(
         ":2042400000000000000000000000000000000000000000000000000000000000000000005E\n")
-    f.write(
+    file.write(
         ":2042600000000000000000000000000000000000000000000000000000000000000000003E\n")
-    f.write(":00000001FF\n")
+    file.write(":00000001FF\n")
 
-    f.close()
+    file.close()
 
 
-# adds missing zero's to the given line to satisfy the hex file
 def add_missing_zeros(line):
     required_len = 73  # number of characters in a line in a hex file
 
@@ -114,8 +113,8 @@ def main():
     if os.path.isfile(file) == 0:
         create_production_hex(file)
 
-    with open(file) as f:
-        file_content = f.readlines()
+    with open(file) as file:
+        file_content = file.readlines()
 
     file_content[1] = ':20402000' + \
         ''.join(map(str, get_computer_name_in_hex()))
@@ -127,8 +126,8 @@ def main():
         file_content[i] += calculate_hex_checksum(file_content[i])
         file_content[i] += '\n'
 
-    with open(file, 'w') as f:
-        f.writelines(file_content)
+    with open(file, 'w') as file:
+        file.writelines(file_content)
 
 
 if __name__ == "__main__":
